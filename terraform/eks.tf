315a800f1385fd691d7bba0d068f3960c999afc6
Ubuntu <ubuntu@ip-10-0-1-96.ec2.internal>
2025-10-09 11:13:13 +0000

    Update EKS Cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,17 +4,13 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
-
-  name = "finops-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
+  name    = "finops-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
-
   enable_nat_gateway = true
   single_nat_gateway = true
-
   tags = {
     Name = "finops-vpc"
   }
@@ -26,15 +22,11 @@ module "vpc" {
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.3.2"
-
-  cluster_name = "finops-eks"
-
-  vpc_id     = module.vpc.vpc_id
+  name    = "finops-eks"  # Changed from cluster_name to name
+  vpc_id  = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
-  enable_irsa                              = true
+  enable_irsa = true
   enable_cluster_creator_admin_permissions = true
-
   eks_managed_node_groups = {
     default = {
       desired_size   = 2
@@ -43,7 +35,6 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
-
   tags = {
     Environment = "demo"
     Project     = "FinOps-Kit"
